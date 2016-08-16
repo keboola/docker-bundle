ffe4b61c0d47c7e5f13a9561af9fd9b4054adf36
Odin <odinuv@gmail.com>
2016-08-16 17:37:19 +0200

    tests: added tests for image parameters

--- Tests/Functional/RunnerTest.php
+++ Tests/Functional/RunnerTest.php
@@ -2,18 +2,22 @@
 
 namespace Keboola\DockerBundle\Tests\Functional;
 
+use Keboola\Csv\CsvFile;
 use Keboola\DockerBundle\Encryption\ComponentProjectWrapper;
 use Keboola\DockerBundle\Encryption\ComponentWrapper;
 use Keboola\DockerBundle\Monolog\ContainerLogger;
 use Keboola\DockerBundle\Service\LoggersService;
 use Keboola\DockerBundle\Service\Runner;
 use Keboola\StorageApi\Client;
+use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\Options\FileUploadOptions;
 use Keboola\StorageApi\Options\ListFilesOptions;
+use Keboola\Syrup\Encryption\BaseWrapper;
 use Keboola\Syrup\Service\ObjectEncryptor;
 use Keboola\Syrup\Service\StorageApi\StorageApiService;
 use Keboola\Temp\Temp;
 use Monolog\Handler\NullHandler;
+use Monolog\Handler\TestHandler;
 use Monolog\Logger;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -29,72 +33,73 @@ class RunnerTest extends KernelTestCase
      */
     private $temp;
 
+    protected function clearBuckets()
+    {
+        foreach (['in.c-docker-test', 'out.c-docker-test'] as $bucket) {
+            try {
+                $this->client->dropBucket($bucket, ['force' => true]);
+            } catch (ClientException $e) {
+                if ($e->getCode() != 404) {
+                    throw $e;
+                }
+            }
+        }
+    }
+
     public function setUp()
     {
-        $this->client = new Client(["token" => STORAGE_API_TOKEN]);
+        parent::setUp();
+        $this->client = new Client(['token' => STORAGE_API_TOKEN]);
         $this->temp = new Temp('docker');
         $this->temp->initRunFolder();
-        if ($this->client->bucketExists("in.c-docker-test")) {
-            // Delete tables
-            foreach ($this->client->listTables("in.c-docker-test") as $table) {
-                $this->client->dropTable($table["id"]);
-            }
-
-            // Delete bucket
-            $this->client->dropBucket("in.c-docker-test");
-        }
-        if ($this->client->bucketExists("out.c-docker-test")) {
-            // Delete tables
-            foreach ($this->client->listTables("out.c-docker-test") as $table) {
-                $this->client->dropTable($table["id"]);
-            }
-
-            // Delete bucket
-            $this->client->dropBucket("out.c-docker-test");
-        }
+        $this->clearBuckets();
 
         // Create buckets
-        $this->client->createBucket("docker-test", Client::STAGE_IN, "Docker TestSuite");
-        $this->client->createBucket("docker-test", Client::STAGE_OUT, "Docker TestSuite");
+        $this->client->createBucket('docker-test', Client::STAGE_IN, 'Docker TestSuite');
+        $this->client->createBucket('docker-test', Client::STAGE_OUT, 'Docker TestSuite');
 
         // remove uploaded files
         $options = new ListFilesOptions();
-        $options->setTags(["docker-bundle-test"]);
+        $options->setTags(['docker-bundle-test']);
         $files = $this->client->listFiles($options);
         foreach ($files as $file) {
-            $this->client->deleteFile($file["id"]);
+            $this->client->deleteFile($file['id']);
         }
 
         self::bootKernel();
     }
 
-    private function getRunner()
+    public function tearDown()
+    {
+        $this->clearBuckets();
+        parent::tearDown();
+    }
+
+    private function getRunner($handler, &$encryptor = null)
     {
         $tokenInfo = $this->client->verifyToken();
-        $tokenData = $this->client->verifyToken();
-
         $storageServiceStub = $this->getMockBuilder(StorageApiService::class)
             ->disableOriginalConstructor()
             ->getMock();
         $storageServiceStub->expects($this->any())
-            ->method("getClient")
+            ->method('getClient')
             ->will($this->returnValue($this->client));
         $storageServiceStub->expects($this->any())
-            ->method("getTokenData")
-            ->will($this->returnValue($tokenData));
+            ->method('getTokenData')
+            ->will($this->returnValue($tokenInfo));
 
-        $log = new Logger("null");
+        $log = new Logger('null');
         $log->pushHandler(new NullHandler());
-        $containerLogger = new ContainerLogger("null");
-        $containerLogger->pushHandler(new NullHandler());
+        $containerLogger = new ContainerLogger('null');
+        $containerLogger->pushHandler($handler);
         $loggersServiceStub = $this->getMockBuilder(LoggersService::class)
             ->disableOriginalConstructor()
             ->getMock();
         $loggersServiceStub->expects($this->any())
-            ->method("getLog")
+            ->method('getLog')
             ->will($this->returnValue($log));
         $loggersServiceStub->expects($this->any())
-            ->method("getContainerLog")
+            ->method('getContainerLog')
             ->will($this->returnValue($containerLogger));
 
         $encryptor = new ObjectEncryptor();
@@ -102,9 +107,10 @@ class RunnerTest extends KernelTestCase
         $ecWrapper->setComponentId('keboola.r-transformation');
         $ecpWrapper = new ComponentProjectWrapper(hash('sha256', uniqid()));
         $ecpWrapper->setComponentId('keboola.r-transformation');
-        $ecpWrapper->setProjectId($tokenInfo["owner"]["id"]);
+        $ecpWrapper->setProjectId($tokenInfo['owner']['id']);
         $encryptor->pushWrapper($ecWrapper);
         $encryptor->pushWrapper($ecpWrapper);
+        $encryptor->pushWrapper(new BaseWrapper(hash('sha256', uniqid())));
 
         /** @var StorageApiService $storageServiceStub */
         /** @var LoggersService $loggersServiceStub */
@@ -119,14 +125,14 @@ class RunnerTest extends KernelTestCase
 
     public function testRunnerPipeline()
     {
-        $dataDir = ROOT_PATH . DIRECTORY_SEPARATOR . "Tests" . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR;
+        $dataDir = ROOT_PATH . DIRECTORY_SEPARATOR . 'Tests' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR;
         $this->client->uploadFile(
             $dataDir . 'texty.zip',
-            (new FileUploadOptions())->setTags(["docker-bundle-test", "pipeline"])
+            (new FileUploadOptions())->setTags(['docker-bundle-test', 'pipeline'])
         );
         $this->client->uploadFile(
             $dataDir . 'radio.zip',
-            (new FileUploadOptions())->setTags(["docker-bundle-test", "pipeline"])
+            (new FileUploadOptions())->setTags(['docker-bundle-test', 'pipeline'])
         );
 
         $configurationData = [
@@ -162,15 +168,15 @@ class RunnerTest extends KernelTestCase
             'processors' => [
                 'before' => [
                     [
-                        "definition" => [
-                            "component" => "keboola.processor.unzipper",
+                        'definition' => [
+                            'component' => 'keboola.processor.unzipper',
                         ],
                     ],
                     [
-                        "definition" => [
-                            "component" => "keboola.processor.iconv",
+                        'definition' => [
+                            'component' => 'keboola.processor.iconv',
                         ],
-                        "parameters" => ['source_encoding' => 'CP1250']
+                        'parameters' => ['source_encoding' => 'CP1250']
                     ],
                 ],
             ],
@@ -187,15 +193,15 @@ class RunnerTest extends KernelTestCase
             'ico32' => '',
             'ico64' => '',
             'data' => [
-                "definition" => [
-                    "type" => "quayio",
-                    "uri" => "keboola/r-transformation",
-                    "tag" => "0.0.14",
+                'definition' => [
+                    'type' => 'quayio',
+                    'uri' => 'keboola/r-transformation',
+                    'tag' => '0.0.14',
                 ],
             ],
         ];
 
-        $runner = $this->getRunner();
+        $runner = $this->getRunner(new NullHandler());
         $runner->run(
             $componentData,
             uniqid('test-'),
@@ -220,5 +226,167 @@ class RunnerTest extends KernelTestCase
         $this->assertArrayHasKey('title', $data[0]);
         $this->assertArrayHasKey('text', $data[0]);
         $this->assertArrayHasKey('tags', $data[0]);
+    }
+
+
+
+
+
+
+
+
+    /*
+    // Create bucket
+    if (!$this->client->bucketExists('in.c-docker-test')) {
+        $this->client->createBucket('docker-test', Client::STAGE_IN, 'Docker Testsuite');
+    }
+
+    // Create table
+    if (!$this->client->tableExists('in.c-docker-test.test')) {
+        $csv = new CsvFile($this->temp->getTmpFolder() . '/upload.csv');
+        $csv->writeRow(['id', 'text']);
+        $csv->writeRow(['test', 'test']);
+        $this->client->createTableAsync('in.c-docker-test', 'test', $csv);
+        $this->client->setTableAttribute('in.c-docker-test.test', 'attr1', 'val1');
+        unset($csv);
+    }
+    */
+
+
+
+    public function testImageParametersDecrypt()
+    {
+        $configurationData = [
+            'storage' => [],
+            'parameters' => [
+                'primary_key_column' => 'id',
+                'data_column' => 'text',
+                'string_length' => '4'
+            ]
+        ];
+        $handler = new TestHandler();
+        $runner = $this->getRunner($handler, $encryptor);
+        /** @var ObjectEncryptor $encryptor */
+        $encrypted = $encryptor->encrypt('someString');
+
+        $componentData = [
+            'id' => 'docker-dummy-component',
+            'type' => 'other',
+            'name' => 'Docker Pipe test',
+            'description' => 'Testing Docker',
+            'longDescription' => null,
+            'hasUI' => false,
+            'hasRun' => true,
+            'ico32' => '',
+            'ico64' => '',
+            'data' => [
+                "definition" => [
+                    "type" => "builder",
+                    "uri" => "quay.io/keboola/docker-base-php56:0.0.2",
+                    "build_options" => [
+                        "repository" => [
+                            "uri" => "https://github.com/keboola/docker-demo-app.git",
+                            "type" => "git"
+                        ],
+                        "commands" => [],
+                        "entry_point" => "cat /data/config.json",
+                    ],
+                ],
+                'configuration_format' => 'json',
+                'image_parameters' => [
+                    'foo' => 'bar',
+                    'baz' => [
+                        'lily' => 'pond'
+                    ],
+                    '#encrypted' => $encrypted
+                ]
+            ],
+        ];
+
+        $runner->run(
+            $componentData,
+            uniqid('test-'),
+            $configurationData,
+            [],
+            'run',
+            'run',
+            '1234567'
+        );
+
+        $ret = $handler->getRecords();
+        $this->assertEquals(1, count($ret));
+        $this->assertArrayHasKey('message', $ret[0]);
+        $config = json_decode($ret[0]['message'], true);
+        $this->assertEquals('bar', $config['image_parameters']['foo']);
+        $this->assertEquals('pond', $config['image_parameters']['baz']['lily']);
+        $this->assertEquals('someString', $config['image_parameters']['#encrypted']);
+    }
+
+    public function testImageParametersNoDecrypt()
+    {
+        $configurationData = [
+            'storage' => [],
+            'parameters' => [
+                'primary_key_column' => 'id',
+                'data_column' => 'text',
+                'string_length' => '4'
+            ]
+        ];
+        $handler = new TestHandler();
+        $runner = $this->getRunner($handler, $encryptor);
+        /** @var ObjectEncryptor $encryptor */
+        $encrypted = $encryptor->encrypt('someString');
+
+        $componentData = [
+            'id' => 'docker-dummy-component',
+            'type' => 'other',
+            'name' => 'Docker Pipe test',
+            'description' => 'Testing Docker',
+            'longDescription' => null,
+            'hasUI' => false,
+            'hasRun' => true,
+            'ico32' => '',
+            'ico64' => '',
+            'data' => [
+                "definition" => [
+                    "type" => "builder",
+                    "uri" => "quay.io/keboola/docker-base-php56:0.0.2",
+                    "build_options" => [
+                        "repository" => [
+                            "uri" => "https://github.com/keboola/docker-demo-app.git",
+                            "type" => "git"
+                        ],
+                        "commands" => [],
+                        "entry_point" => "cat /data/config.json",
+                    ],
+                ],
+                'configuration_format' => 'json',
+                'image_parameters' => [
+                    'foo' => 'bar',
+                    'baz' => [
+                        'lily' => 'pond'
+                    ],
+                    '#encrypted' => $encrypted
+                ]
+            ],
+        ];
+
+        $runner->run(
+            $componentData,
+            uniqid('test-'),
+            $configurationData,
+            [],
+            'run',
+            'dry-run',
+            '1234567'
+        );
+
+        $ret = $handler->getRecords();
+        $this->assertEquals(1, count($ret));
+        $this->assertArrayHasKey('message', $ret[0]);
+        $config = json_decode($ret[0]['message'], true);
+        $this->assertEquals('bar', $config['image_parameters']['foo']);
+        $this->assertEquals('pond', $config['image_parameters']['baz']['lily']);
+        $this->assertEquals($encrypted, $config['image_parameters']['#encrypted']);
     }
 }
--- Tests/Runner/ConfigFileTest.php
+++ Tests/Runner/ConfigFileTest.php
@@ -63,4 +63,52 @@ SAMPLE;
             $this->assertContains('Error in configuration: Unrecognized option', $e->getMessage());
         }
     }
+
+    public function testDefinitionParameters()
+    {
+        $imageConfig = [
+            "definition" => [
+                "type" => "dockerhub",
+                "uri" => "keboola/docker-demo"
+            ],
+            "configuration_format" => "yaml",
+        ];
+
+        $configData = [
+            "storage" => [
+            ],
+            "parameters" => [
+                "primary_key_column" => "id",
+                "data_column" => "text",
+                "string_length" => "4"
+            ],
+            "runtime" => [
+                "foo" => "bar",
+                "baz" => "next"
+            ]
+        ];
+
+        $temp = new Temp();
+        $encryptor = new ObjectEncryptor();
+        $encryptor->pushWrapper(new BaseWrapper(md5(uniqid())));
+
+        $oauthClientStub = $this->getMockBuilder(Credentials::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $authorization = new Authorization($oauthClientStub, $encryptor, 'dummy-component', false);
+        $config = new ConfigFile($temp->getTmpFolder(), $imageConfig, $authorization, 'run', 'json');
+        $config->createConfigFile($configData);
+        $config = json_decode(file_get_contents($temp->getTmpFolder() . DIRECTORY_SEPARATOR . 'config.json'), true);
+
+        $this->assertEquals('id', $config['parameters']['primary_key_column']);
+        $this->assertEquals('text', $config['parameters']['data_column']);
+        $this->assertEquals('4', $config['parameters']['string_length']);
+        // volatile parameters must not get stored
+        $this->assertArrayNotHasKey('foo', $config['parameters']);
+        $this->assertArrayNotHasKey('baz', $config['parameters']);
+    }
+
+
+
+
 }
