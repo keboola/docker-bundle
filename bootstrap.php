f77cf0bc26b2da5fc04223e6697e02efdeac0c0f
Odin <odinuv@gmail.com>
2016-11-12 01:05:17 +0100

    tests: add environment consts and CF test stack

--- bootstrap.php
+++ bootstrap.php
@@ -12,28 +12,37 @@ if (file_exists(__DIR__ . '/config.php')) {
 require_once ROOT_PATH . '/vendor/autoload.php';
 
 defined('STORAGE_API_URL')
-|| define('STORAGE_API_URL', getenv('STORAGE_API_URL') ? getenv('STORAGE_API_URL') : 'sapi_url');
+|| define('STORAGE_API_URL', getenv('STORAGE_API_URL') ?: 'sapi_url');
 
 defined('STORAGE_API_TOKEN')
-    || define('STORAGE_API_TOKEN', getenv('STORAGE_API_TOKEN') ? getenv('STORAGE_API_TOKEN') : 'your_token');
+    || define('STORAGE_API_TOKEN', getenv('STORAGE_API_TOKEN') ?: 'your_token');
 
 defined('DOCKERHUB_PRIVATE_USERNAME')
-    || define('DOCKERHUB_PRIVATE_USERNAME', getenv('DOCKERHUB_PRIVATE_USERNAME') ? getenv('DOCKERHUB_PRIVATE_USERNAME') : 'username');
+    || define('DOCKERHUB_PRIVATE_USERNAME', getenv('DOCKERHUB_PRIVATE_USERNAME') ?: 'username');
 
 defined('DOCKERHUB_PRIVATE_PASSWORD')
-    || define('DOCKERHUB_PRIVATE_PASSWORD', getenv('DOCKERHUB_PRIVATE_PASSWORD') ? getenv('DOCKERHUB_PRIVATE_PASSWORD') : 'password');
+    || define('DOCKERHUB_PRIVATE_PASSWORD', getenv('DOCKERHUB_PRIVATE_PASSWORD') ?: 'password');
 
 defined('DOCKERHUB_PRIVATE_SERVER')
-    || define('DOCKERHUB_PRIVATE_SERVER', getenv('DOCKERHUB_PRIVATE_SERVER') ? getenv('DOCKERHUB_PRIVATE_SERVER') : 'server');
+    || define('DOCKERHUB_PRIVATE_SERVER', getenv('DOCKERHUB_PRIVATE_SERVER') ?: 'server');
 
 defined('GIT_PRIVATE_USERNAME')
-    || define('GIT_PRIVATE_USERNAME', getenv('GIT_PRIVATE_USERNAME') ? getenv('GIT_PRIVATE_USERNAME') : 'username');
+    || define('GIT_PRIVATE_USERNAME', getenv('GIT_PRIVATE_USERNAME') ?: 'username');
 
 defined('GIT_PRIVATE_PASSWORD')
-    || define('GIT_PRIVATE_PASSWORD', getenv('GIT_PRIVATE_PASSWORD') ? getenv('GIT_PRIVATE_PASSWORD') : 'password');
+    || define('GIT_PRIVATE_PASSWORD', getenv('GIT_PRIVATE_PASSWORD') ?: 'password');
 
 defined('QUAYIO_PRIVATE_USERNAME')
-    || define('QUAYIO_PRIVATE_USERNAME', getenv('QUAYIO_PRIVATE_USERNAME') ? getenv('QUAYIO_PRIVATE_USERNAME') : 'username');
+    || define('QUAYIO_PRIVATE_USERNAME', getenv('QUAYIO_PRIVATE_USERNAME') ?: 'username');
 
 defined('QUAYIO_PRIVATE_PASSWORD')
-    || define('QUAYIO_PRIVATE_PASSWORD', getenv('QUAYIO_PRIVATE_PASSWORD') ? getenv('QUAYIO_PRIVATE_PASSWORD') : 'password');
+    || define('QUAYIO_PRIVATE_PASSWORD', getenv('QUAYIO_PRIVATE_PASSWORD') ?: 'password');
+
+defined('AWS_ECR_REGISTRY_URI')
+|| define('AWS_ECR_REGISTRY_URI', getenv('AWS_ECR_REGISTRY_URI') ?: 'foobar.amazon.com');
+
+defined('AWS_ECR_ACCESS_KEY_ID')
+|| define('AWS_ECR_ACCESS_KEY_ID', getenv('AWS_ECR_ACCESS_KEY_ID') ?: 'key');
+
+defined('AWS_ECR_SECRET_ACCESS_KEY')
+|| define('AWS_ECR_SECRET_ACCESS_KEY', getenv('AWS_ECR_SECRET_ACCESS_KEY') ?: 'secret');
