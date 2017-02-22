6fcedf89ae37a9192ea0d4d92f3b8f736864881d
pivnicek <marc@keboola.com>
2017-02-22 11:19:24 +0100

    test: metadata in table manifest

--- Tests/Docker/OutputTableManifestConfigurationTest.php
+++ Tests/Docker/OutputTableManifestConfigurationTest.php
@@ -25,7 +25,9 @@ class OutputTableManifestConfigurationTest extends \PHPUnit_Framework_TestCase
             "delete_where_operator" => "eq",
             "delimiter" => ",",
             "enclosure" => "\"",
-            "escaped_by" => ""
+            "escaped_by" => "",
+            "metadata" => array(),
+            "columnMetadata" => array()
         );
 
         $processedConfiguration = (new Table\Manifest())->parse(array("config" => $config));
@@ -47,7 +49,9 @@ class OutputTableManifestConfigurationTest extends \PHPUnit_Framework_TestCase
             "delete_where_operator" => "ne",
             "delimiter" => "\t",
             "enclosure" => "'",
-            "escaped_by" => "\\"
+            "escaped_by" => "\\",
+            "metadata" => array(),
+            "columnMetadata" => array()
         );
 
         $expectedArray = $config;
@@ -68,5 +72,88 @@ class OutputTableManifestConfigurationTest extends \PHPUnit_Framework_TestCase
             "delete_where_operator" => 'abc'
         );
         (new Table\Manifest())->parse(array("config" => $config));
+    }
+
+    public function testTableMetadataConfiguration()
+    {
+        $config = array(
+            "destination" => "in.c-main.test",
+            "metadata" => [
+                [
+                    "key" => "table.key.one",
+                    "value" => "table value one"
+                ],
+                [
+                    "key" => 'table.key.two',
+                    "value" => "table value two"
+                ]
+            ]
+        );
+
+        $expectedArray = array(
+            "destination" => "in.c-main.test",
+            "primary_key" => array(),
+            "columns" => array(),
+            "incremental" => false,
+            "delete_where_values" => array(),
+            "delete_where_operator" => "eq",
+            "delimiter" => ",",
+            "enclosure" => "\"",
+            "escaped_by" => "",
+            "columnMetadata" => array()
+        );
+        $expectedArray['metadata'] = $config['metadata'];
+
+        $parsedConfig = (new Table\Manifest())->parse(array("config" => $config));
+
+        $this->assertEquals($expectedArray, $parsedConfig);
+    }
+
+    public function testColumnMetadataConfiguration()
+    {
+        $config = array(
+            "destination" => "in.c-main.test",
+            "columnMetadata" => [
+                "colA" => [
+                    [
+                        "key" => "column.key.one",
+                        "value" => "column value A"
+                    ],
+                    [
+                        "key" => "column.key.two",
+                        "value" => "column value A"
+                    ]
+                ],
+                "colB" => [
+                    [
+                        "key" => "column.key.one",
+                        "value" => "column value B"
+                    ],
+                    [
+                        "key" => "column.key.two",
+                        "value" => "column value B"
+                    ]
+                ]
+            ]
+        );
+
+        $expectedArray = array(
+            "destination" => "in.c-main.test",
+            "primary_key" => array(),
+            "columns" => array(),
+            "incremental" => false,
+            "delete_where_values" => array(),
+            "delete_where_operator" => "eq",
+            "delimiter" => ",",
+            "enclosure" => "\"",
+            "escaped_by" => "",
+            "metadata" => array()
+        );
+
+        $expectedArray['columnMetadata'] = $config['columnMetadata'];
+
+        $parsedConfig = (new Table\Manifest())->parse(array("config" => $config));
+
+        $this->assertEquals($expectedArray, $parsedConfig);
     }
 }
