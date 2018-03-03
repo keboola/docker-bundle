c13b55b24fefa1fdcf3b818819c971ddaa0fb2b5
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-03-03 21:15:42 +0100

    fix: coding style

--- Service/StorageApiService.php
+++ Service/StorageApiService.php
@@ -11,7 +11,7 @@ class StorageApiService extends \Keboola\Syrup\Service\StorageApi\StorageApiServ
      */
     public static function getStepPollDelayFunction()
     {
-        return function($tries) {
+        return function ($tries) {
             switch ($tries) {
                 case ($tries < 15):
                     return 1;
