0dd11747ffd7d58ed1715cdb1ff800bd2be50645
Odin <odinuv@gmail.com>
2018-05-09 12:50:56 +0200

    fix: calling convention

--- Tests/Functional/RunnerTest.php
+++ Tests/Functional/RunnerTest.php
@@ -929,6 +929,7 @@ class RunnerTest extends KernelTestCase
             $loggersServiceStub,
             $jobMapperStub,
             "dummy",
+            ['cpu_count' => 2],
             RUNNER_COMMAND_TO_GET_HOST_IP,
             RUNNER_MIN_LOG_PORT,
             RUNNER_MAX_LOG_PORT
