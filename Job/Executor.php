daa8e1c0c340a8b5d0e4d13d773f49f22c72b13c
Odin <odinuv@gmail.com>
2018-05-10 09:29:29 +0200

    perf: do not call tokenInfo twice

--- Job/Executor.php
+++ Job/Executor.php
@@ -100,7 +100,7 @@ class Executor extends BaseExecutor
     public function execute(Job $job)
     {
         $this->tokenInfo = $this->storageApi->verifyToken();
-        $this->runner->setFeatures($this->tokenInfo["owner"]["features"]);
+        $this->runner->setTokenInfo($this->tokenInfo);
         $this->encryptorFactory->setProjectId($this->tokenInfo["owner"]["id"]);
         if (isset($job->getRawParams()["component"])) {
             $this->encryptorFactory->setComponentId($job->getRawParams()["component"]);
