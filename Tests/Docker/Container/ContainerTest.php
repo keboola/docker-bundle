e15ac9d6eea4734ea846cae6a138e14cef73215c
Odin <odinuv@gmail.com>
2018-05-23 12:06:34 +0200

    tests: this is not used at all

--- Tests/Docker/Container/ContainerTest.php
+++ Tests/Docker/Container/ContainerTest.php
@@ -78,42 +78,6 @@ class ContainerTest extends \PHPUnit_Framework_TestCase
         };
 
         $container->setRunMethod($callback);
-        $configFile = <<< EOT
-{
-    "storage": {
-        "input": {
-            "tables": {
-                "0": {
-                    "source": "in.c-main.data"
-                }
-            }
-        },
-        "output": {
-            "tables": {
-                "0": {
-                    "source": "sliced.csv",
-                    "destination": "out.c-main.data"
-                }
-            }
-        }
-    },
-    "parameters": {
-        "primary_key_column": "id",
-        "data_column": "text",
-        "string_length": 10
-    }
-}
-EOT;
-        file_put_contents($dataDir . DIRECTORY_SEPARATOR . "config.json", $configFile);
-
-        $dataFile = <<< EOF
-id,text,some_other_column
-1,"Short text","Whatever"
-2,"Long text Long text Long text","Something else"
-EOF;
-
-        file_put_contents($tableDir . DIRECTORY_SEPARATOR . 'in.c-main.data.csv', $dataFile);
-
         $process = $container->run();
         $this->assertEquals(0, $process->getExitCode());
         $this->assertContains("Processed 2 rows.", trim($process->getOutput()));
