def18989a053bd7bb6568224c31a3025429c7f52
Ondrej Popelka <odinuv@gmail.com>
2018-05-24 16:27:04 +0200

    Merge pull request #307 from keboola/najlos-container-user
    feat: container-root-user feature
    # Conflicts:
    #	Tests/Docker/ContainerTest.php
    #	Tests/Functional/NetworkTest.php
    #	Tests/Functional/RunnerTest.php

--- Tests/Runner/RunnerConfigRowsTest.php
+++ Tests/Runner/RunnerConfigRowsTest.php
@@ -116,7 +116,7 @@ class RunnerConfigRowsTest extends BaseRunnerTest
 
     private function getComponentWithContainerRootUserFeature()
     {
-        $componentData = [
+        return new Component([
             'id' => 'docker-demo',
             'type' => 'other',
             'name' => 'Docker State test',
@@ -143,11 +143,9 @@ class RunnerConfigRowsTest extends BaseRunnerTest
                             . 'chmod 000 /data/out/tables/mytable.csv.gz/part2'
                     ],
                 ],
-                'configuration_format' => 'json',
             ],
             'features' => ['container-root-user']
-        ];
-        return new Component($componentData);
+        ]);
     }
 
     public function testRunMultipleRows()
@@ -192,7 +190,7 @@ class RunnerConfigRowsTest extends BaseRunnerTest
 
     public function testRunMultipleRowsWithContainerRootUserFeature()
     {
-        $runner = $this->getRunner($this->getLoggersServiceStub());
+        $runner = $this->getRunner();
         $jobDefinition1 = new JobDefinition([
             "storage" => [
                 "output" => [
@@ -226,8 +224,8 @@ class RunnerConfigRowsTest extends BaseRunnerTest
             'run',
             '1234567'
         );
-        $this->assertTrue($this->client->tableExists('in.c-docker-test.mytable'));
-        $this->assertTrue($this->client->tableExists('in.c-docker-test.mytable-2'));
+        self::assertTrue($this->getClient()->tableExists('in.c-docker-test.mytable'));
+        self::assertTrue($this->getClient()->tableExists('in.c-docker-test.mytable-2'));
     }
 
     public function testRunMultipleRowsFiltered()
