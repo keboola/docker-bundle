5b171c8e918edd81a600fa051ce25b4444c21445
Odin <odinuv@gmail.com>
2018-05-24 09:11:47 +0200

    tests: fix calling convention

--- Tests/Docker/Container/ContainerErrorHandlingTest.php
+++ Tests/Docker/Container/ContainerErrorHandlingTest.php
@@ -2,6 +2,7 @@
 
 namespace Keboola\DockerBundle\Tests\Functional;
 
+use Keboola\DockerBundle\Docker\RunCommandOptions;
 use Keboola\DockerBundle\Exception\OutOfMemoryException;
 use Keboola\DockerBundle\Tests\BaseContainerTest;
 use Keboola\Syrup\Exception\ApplicationException;
@@ -25,7 +26,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
     public function testSuccess()
     {
         $script = ['print("Hello from Keboola Space Program")'];
-        $container = $this->getContainer($this->getImageConfiguration(), [], $script);
+        $container = $this->getContainer($this->getImageConfiguration(), [], $script, true);
         $process = $container->run();
 
         self::assertEquals(0, $process->getExitCode());
@@ -35,7 +36,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
     public function testFatal()
     {
         $script = ['import sys', 'print("application error")', 'sys.exit(2)'];
-        $container = $this->getContainer($this->getImageConfiguration(), [], $script);
+        $container = $this->getContainer($this->getImageConfiguration(), [], $script, true);
         
         self::expectException(ApplicationException::class);
         self::expectExceptionMessage('application error');
@@ -45,7 +46,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
     public function testGraceful()
     {
         $script = ['import sys', 'print("user error")', 'sys.exit(1)'];
-        $container = $this->getContainer($this->getImageConfiguration(), [], $script);
+        $container = $this->getContainer($this->getImageConfiguration(), [], $script, true);
         
         self::expectException(UserException::class);
         self::expectExceptionMessage('user error');
@@ -55,7 +56,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
     public function testLessGraceful()
     {
         $script = ['import sys', 'print("less graceful error")', 'sys.exit(255)'];
-        $container = $this->getContainer($this->getImageConfiguration(), [], $script);
+        $container = $this->getContainer($this->getImageConfiguration(), [], $script, true);
 
         self::expectException(ApplicationException::class);
         self::expectExceptionMessage('graceful error');
@@ -66,7 +67,8 @@ class ContainerErrorHandlingTest extends BaseContainerTest
     {
         $script = ['import os', 'print(os.environ["KBC_TOKENID"])'];
         $value = '123 ščř =-\'"321';
-        $container = $this->getContainer($this->getImageConfiguration(), ['KBC_TOKENID' => $value], $script);
+        $commandOptions = new RunCommandOptions([], ['KBC_TOKENID' => $value]);
+        $container = $this->getContainer($this->getImageConfiguration(), $commandOptions, $script, true);
 
         $process = $container->run();
         self::assertContains($value, $process->getOutput());
@@ -78,7 +80,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
         $imageConfiguration = $this->getImageConfiguration();
         $imageConfiguration['data']['process_timeout'] = $timeout;
         $script = ['print("done")'];
-        $container = $this->getContainer($imageConfiguration, [], $script);
+        $container = $this->getContainer($imageConfiguration, [], $script, true);
 
         // set benchmark time
         $benchmarkStartTime = time();
@@ -87,7 +89,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
 
         // actual test
         $script = ['import time', 'time.sleep(20)'];
-        $container = $this->getContainer($imageConfiguration, [], $script);
+        $container = $this->getContainer($imageConfiguration, [], $script, true);
         $testStartTime = time();
         try {
             $container->run();
@@ -106,7 +108,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
     {
         // check that the container can run longer than the default 60s symfony process timeout
         $script = ['import time', 'time.sleep(80)'];
-        $container = $this->getContainer($this->getImageConfiguration(), [], $script);
+        $container = $this->getContainer($this->getImageConfiguration(), [], $script, true);
         $process = $container->run();
         self::assertEquals(0, $process->getExitCode());
     }
@@ -117,7 +119,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
         $imageConfiguration['data']['definition']['uri'] = '147946154733.dkr.ecr.us-east-1.amazonaws.com/developer-portal-v2/non-existent';
         self::expectExceptionMessage('Cannot pull');
         self::expectException(ApplicationException::class);
-        $this->getContainer($imageConfiguration, [], []);
+        $this->getContainer($imageConfiguration, [], [], true);
     }
 
     public function testOutOfMemory()
@@ -128,7 +130,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
         $imageConfiguration = $this->getImageConfiguration();
         $imageConfiguration['data']['memory'] = '32m';
         $script = ['list = []', 'for i in range(100000000):', '   list.append("0123456789")'];
-        $container = $this->getContainer($imageConfiguration, [], $script);
+        $container = $this->getContainer($imageConfiguration, [], $script, true);
         $container->run();
     }
 
@@ -154,7 +156,7 @@ class ContainerErrorHandlingTest extends BaseContainerTest
             ]
         ];
         $script = ['list = []', 'for i in range(100000000):', '   list.append("0123456789")'];
-        $container = $this->getContainer($imageConfiguration, [], $script);
+        $container = $this->getContainer($imageConfiguration, [], $script, true);
         self::expectException(OutOfMemoryException::class);
         self::expectExceptionMessage('Component out of memory');
         $container->run();
