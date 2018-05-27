f6a53b2ce849b5589bb2f645d26c75b4dd86dda2
Odin <odinuv@gmail.com>
2018-05-27 10:35:44 +0200

    tests: move to a better place

--- Tests/Runner/RunnerTest.php
+++ Tests/Runner/RunnerTest.php
@@ -11,10 +11,13 @@ use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\Components;
 use Keboola\StorageApi\Metadata;
 use Keboola\StorageApi\Options\Components\Configuration;
+use Keboola\StorageApi\Options\Components\ConfigurationRow;
 use Keboola\StorageApi\Options\FileUploadOptions;
 use Keboola\StorageApi\Options\ListFilesOptions;
+use Keboola\Syrup\Elasticsearch\JobMapper;
 use Keboola\Syrup\Exception\ApplicationException;
 use Keboola\Syrup\Exception\UserException;
+use Keboola\Syrup\Job\Metadata\Job;
 use Keboola\Temp\Temp;
 use Monolog\Logger;
 
@@ -1584,5 +1587,133 @@ class RunnerTest extends BaseRunnerTest
         }
         self::assertNotContains(STORAGE_API_TOKEN, $output);
         self::assertContains('[hidden]', $output);
+    }
+
+    public function testExecutorStoreUsage()
+    {
+        $job = new Job($this->getEncryptorFactory()->getEncryptor());
+        $jobMapperStub = self::getMockBuilder(JobMapper::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['get', 'update'])
+            ->getMock();
+        $jobMapperStub->expects(self::once())
+            ->method('get')
+            ->with('987654')
+            ->willReturn($job);
+        $this->setJobMapperMock($jobMapperStub);
+        $component = new Components($this->getClient());
+        try {
+            $component->deleteConfiguration('docker-demo', 'test-configuration');
+        } catch (ClientException $e) {
+            if ($e->getCode() != 404) {
+                throw $e;
+            }
+        }
+        $configuration = new Configuration();
+        $configuration->setComponentId('docker-demo');
+        $configuration->setName('Test configuration');
+        $configuration->setConfigurationId('test-configuration');
+        $component->addConfiguration($configuration);
+        $componentData = [
+            'id' => 'keboola.docker-demo-sync',
+            'data' => [
+                'definition' => [
+                    'type' => 'aws-ecr',
+                    'uri' => '147946154733.dkr.ecr.us-east-1.amazonaws.com/developer-portal-v2/keboola.python-transformation',
+                ],
+            ],
+        ];
+        $configData = [
+            'parameters' => [
+                'script' => [
+                    'with open("/data/out/usage.json", "w") as file:',
+                    '   file.write(\'[{"metric": "kB", "value": 150}]\')',
+                ],
+            ],
+        ];
+        $jobDefinition = new JobDefinition($configData, new Component($componentData), 'test-configuration');
+        $runner = $this->getRunner();
+        $runner->run([$jobDefinition], 'run', 'run', '987654');
+        self::assertEquals([
+            [
+                'metric' => 'kB',
+                'value' => 150
+            ]
+        ], $job->getUsage());
+
+        $component->deleteConfiguration('docker-demo', 'test-configuration');
+    }
+
+    public function testExecutorStoreRowsUsage()
+    {
+        $job = new Job($this->getEncryptorFactory()->getEncryptor());
+        $jobMapperStub = self::getMockBuilder(JobMapper::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['get', 'update'])
+            ->getMock();
+        $jobMapperStub->expects(self::atLeastOnce())
+            ->method('get')
+            ->with('987654')
+            ->willReturn($job);
+        $this->setJobMapperMock($jobMapperStub);
+
+        $component = new Components($this->getClient());
+        try {
+            $component->deleteConfiguration('docker-demo', 'test-configuration');
+        } catch (ClientException $e) {
+            if ($e->getCode() != 404) {
+                throw $e;
+            }
+        }
+        $configuration = new Configuration();
+        $configuration->setComponentId('docker-demo');
+        $configuration->setName('Test configuration');
+        $configuration->setConfigurationId('test-configuration');
+        $component->addConfiguration($configuration);
+
+        $configurationRow = new ConfigurationRow($configuration);
+        $configurationRow->setRowId('row-1');
+        $configurationRow->setName('Row 1');
+        $component->addConfigurationRow($configurationRow);
+
+        $configurationRow = new ConfigurationRow($configuration);
+        $configurationRow->setRowId('row-2');
+        $configurationRow->setName('Row 2');
+        $component->addConfigurationRow($configurationRow);
+
+        $componentData = [
+            'id' => 'keboola.docker-demo-sync',
+            'data' => [
+                'definition' => [
+                    'type' => 'aws-ecr',
+                    'uri' => '147946154733.dkr.ecr.us-east-1.amazonaws.com/developer-portal-v2/keboola.python-transformation',
+                ],
+            ],
+        ];
+        $configData = [
+            'parameters' => [
+                'script' => [
+                    'with open("/data/out/usage.json", "w") as file:',
+                    '   file.write(\'[{"metric": "kB", "value": 150}]\')',
+                ],
+            ],
+        ];
+
+        $jobDefinition1 = new JobDefinition($configData, new Component($componentData), 'test-configuration', null, [], 'row-1');
+        $jobDefinition2 = new JobDefinition($configData, new Component($componentData), 'test-configuration', null, [], 'row-2');
+        $runner = $this->getRunner();
+        $runner->run([$jobDefinition1, $jobDefinition2], 'run', 'run', '987654');
+        self::assertEquals([
+            [
+                'metric' => 'kB',
+                'value' => 150
+            ],
+            [
+                'metric' => 'kB',
+                'value' => 150
+            ]
+        ], $job->getUsage());
+
+        $component->deleteConfiguration('docker-demo', 'test-configuration');
     }
 }
