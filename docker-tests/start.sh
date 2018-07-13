3388b574732673f196608a1793a9e03170290d40
Odin <odinuv@gmail.com>
2018-07-13 09:14:43 +0200

    wip: librarize

--- docker-tests/start.sh
+++ docker-tests/start.sh
@@ -5,13 +5,13 @@ echo "export DOCKER_HOST DEFAULT=$DOCKER_HOST" >> /etc/security/pam_env.conf
 
 # run composer without scripts copy parameters manually and run install scripts to build bootstrap
 composer install --no-scripts
-cp /buildcode/parameters.yml /code/vendor/keboola/syrup/app/config/
-cp /buildcode/parameters_shared.yml /code/vendor/keboola/syrup/app/config/
-cp /buildcode/config_dev.yml /code/vendor/keboola/syrup/app/config/
-cp /buildcode/config_prod.yml /code/vendor/keboola/syrup/app/config/
-cp /buildcode/config.yml /code/vendor/keboola/syrup/app/config/
-composer run-script build-bootstrap
-php /code/vendor/keboola/syrup/app/console syrup:create-index
+#cp /buildcode/parameters.yml /code/vendor/keboola/syrup/app/config/
+#cp /buildcode/parameters_shared.yml /code/vendor/keboola/syrup/app/config/
+#cp /buildcode/config_dev.yml /code/vendor/keboola/syrup/app/config/
+#cp /buildcode/config_prod.yml /code/vendor/keboola/syrup/app/config/
+#cp /buildcode/config.yml /code/vendor/keboola/syrup/app/config/
+#composer run-script build-bootstrap
+#php /code/vendor/keboola/syrup/app/console syrup:create-index
 
 service ssh start
 mkdir -p /var/run/apache2
