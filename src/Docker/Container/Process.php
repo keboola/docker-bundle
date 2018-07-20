e831ff9845d89be4b451231a9a2da4eb30412817
Odin <odinuv@gmail.com>
2018-07-20 08:47:15 +0200

    fix: can't be there at the moment

--- src/Docker/Container/Process.php
+++ src/Docker/Container/Process.php
@@ -66,57 +66,57 @@ if (version_compare(PHP_VERSION, '7.0.0', '<')) {
         }
     }
 } else {
-    class Process extends \Symfony\Component\Process\Process
-    {
-        /**
-         * @var OutputFilterInterface
-         */
-        private $outputFilter;
-
-        public function __construct($commandline, $cwd = null, array $env = null, $input = null, $timeout = 60, array $options = array())
-        {
-            parent::__construct($commandline, $cwd, $env, $input, $timeout, $options);
-            $this->outputFilter = new NullFilter();
-        }
-
-        /**
-         * @return string
-         */
-        public function getOutput()
-        {
-            return $this->filter(trim(\Keboola\Utils\sanitizeUtf8(parent::getOutput())));
-        }
-
-        /**
-         * @return string
-         */
-        public function getErrorOutput()
-        {
-            return $this->filter(trim(\Keboola\Utils\sanitizeUtf8(parent::getErrorOutput())));
-        }
-
-        /**
-         * @param callable|null $callback
-         * @return int
-         */
-        public function run(callable $callback = null, array $env = array()): int
-        {
-            $myCallback = function ($type, $buffer) use ($callback) {
-                if ($callback) {
-                    $callback($type, $this->filter(trim(\Keboola\Utils\sanitizeUtf8($buffer))));
-                }
-            };
-            return parent::run($myCallback);
-        }
-
-        public function setOutputFilter(OutputFilterInterface $outputFilter)
-        {
-            $this->outputFilter = $outputFilter;
-        }
-
-        private function filter($value)
-        {
-            return $this->outputFilter->filter($value);
-        }
-    }
+//    class Process extends \Symfony\Component\Process\Process
+//    {
+//        /**
+//         * @var OutputFilterInterface
+//         */
+//        private $outputFilter;
+//
+//        public function __construct($commandline, $cwd = null, array $env = null, $input = null, $timeout = 60, array $options = array())
+//        {
+//            parent::__construct($commandline, $cwd, $env, $input, $timeout, $options);
+//            $this->outputFilter = new NullFilter();
+//        }
+//
+//        /**
+//         * @return string
+//         */
+//        public function getOutput()
+//        {
+//            return $this->filter(trim(\Keboola\Utils\sanitizeUtf8(parent::getOutput())));
+//        }
+//
+//        /**
+//         * @return string
+//         */
+//        public function getErrorOutput()
+//        {
+//            return $this->filter(trim(\Keboola\Utils\sanitizeUtf8(parent::getErrorOutput())));
+//        }
+//
+//        /**
+//         * @param callable|null $callback
+//         * @return int
+//         */
+//        public function run(callable $callback = null, array $env = array()): int
+//        {
+//            $myCallback = function ($type, $buffer) use ($callback) {
+//                if ($callback) {
+//                    $callback($type, $this->filter(trim(\Keboola\Utils\sanitizeUtf8($buffer))));
+//                }
+//            };
+//            return parent::run($myCallback);
+//        }
+//
+//        public function setOutputFilter(OutputFilterInterface $outputFilter)
+//        {
+//            $this->outputFilter = $outputFilter;
+//        }
+//
+//        private function filter($value)
+//        {
+//            return $this->outputFilter->filter($value);
+//        }
+//    }
 }
