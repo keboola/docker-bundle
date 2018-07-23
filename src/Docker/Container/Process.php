93253ceadc336632d2809e218766dae2ef40a641
Odin <odinuv@gmail.com>
2018-07-23 08:14:31 +0200

    fix: missing separator

--- src/Docker/Container/Process.php
+++ src/Docker/Container/Process.php
@@ -1,7 +1,7 @@
 <?php
 
 if (version_compare(PHP_VERSION, '7.0.0', '<')) {
-    require __DIR__ . 'process.php5';
+    require __DIR__ . '/process.php5';
 } else {
-    require __DIR__ . 'process.php7';
+    require __DIR__ . '/process.php7';
 }
