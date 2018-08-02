624d5a320776f0f6ec0d8bf4d1097489de4017c1
Odin <odinuv@gmail.com>
2018-08-02 09:21:40 +0200

    fix: revert back StorageApiService because it has to be inherited anyway

--- src/Service/StorageApiService.php
+++ src/Service/StorageApiService.php
@@ -2,49 +2,11 @@
 
 namespace Keboola\DockerBundle\Service;
 
-use Keboola\DockerBundle\Exception\ApplicationException;
-use Keboola\DockerBundle\Exception\NoRequestException;
-use Keboola\DockerBundle\Exception\UserException;
 use Keboola\StorageApi\Client;
-use Keboola\StorageApi\ClientException;
-use Keboola\StorageApi\Exception;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 class StorageApiService extends \Keboola\Syrup\Service\StorageApi\StorageApiService
 {
-    /**
-     * @var RequestStack
-     */
-    protected $requestStack;
-
-    /**
-     * @var Client
-     */
-    protected $client;
-
-    /**
-     * @var string
-     */
-    protected $storageApiUrl;
-
-    /**
-     * @var array
-     */
-    protected $tokenData;
-
-    public function __construct(RequestStack $requestStack, $storageApiUrl)
-    {
-        $this->storageApiUrl = $storageApiUrl;
-        $this->requestStack = $requestStack;
-    }
-
-    public function getTokenData()
-    {
-        if ($this->tokenData == null) {
-            throw new ApplicationException('StorageApi Client was not initialized');
-        }
-        return $this->tokenData;
-    }
+    private $fasterPollingClient = null;
 
     /**
      * @return \Closure
@@ -63,68 +25,32 @@ class StorageApiService extends \Keboola\Syrup\Service\StorageApi\StorageApiServ
         };
     }
 
-    protected function verifyClient(Client $client)
-    {
-        try {
-            $this->tokenData = $client->verifyToken();
-            return $client;
-        } catch (ClientException $e) {
-            if ($e->getCode() == 401) {
-                throw new UserException("Invalid StorageApi Token", $e);
-            } elseif ($e->getCode() < 500) {
-                throw new UserException($e->getMessage(), $e);
-            }
-            throw $e;
-        }
-    }
-
-    public function getBackoffTries($hostname)
-    {
-        // keep the backoff settings minimal for API servers
-        if (false === strstr($hostname, 'worker')) {
-            return 3;
-        }
-
-        return 11;
-    }
-
-    public function setClient(Client $client)
-    {
-        $this->client = $this->verifyClient($client);
-    }
-
     /**
      * @return Client
      */
     public function getClient()
     {
-        $request = $this->requestStack->getCurrentRequest();
-
-        if ($this->client == null) {
-            if ($request == null) {
-                throw new NoRequestException('');
-            }
-
-            if (!$request->headers->has('X-StorageApi-Token')) {
-                throw new Exception('Missing StorageAPI token');
-            }
-
-            $this->setClient(
-                new Client(
-                    [
-                        'token' => $request->headers->get('X-StorageApi-Token'),
-                        'url' => $this->storageApiUrl,
-                        'userAgent' => explode('/', $request->getPathInfo())[1],
-                        'backoffMaxTries' => $this->getBackoffTries(gethostname()),
-                        'jobPollRetryDelay' => self::getStepPollDelayFunction()
-                    ]
-                )
+        $client = parent::getClient();
+        if (!$this->fasterPollingClient) {
+            $clientWithFasterPolling = new Client(
+                [
+                    'token' => $client->token,
+                    'url' => $client->getApiUrl(),
+                    'userAgent' => $client->getUserAgent(),
+                    'backoffMaxTries' => $client->getBackoffMaxTries(),
+                    'jobPollRetryDelay' => self::getStepPollDelayFunction()
+                ]
             );
-
-            if ($request->headers->has('X-KBC-RunId')) {
-                $this->client->setRunId($request->headers->get('X-KBC-RunId'));
+            if ($client->getRunId()) {
+                $clientWithFasterPolling->setRunId($client->getRunId());
             }
+            $this->setFasterPollingClient($clientWithFasterPolling);
         }
-        return $this->client;
+        return $this->fasterPollingClient;
+    }
+
+    public function setFasterPollingClient(Client $client)
+    {
+        $this->fasterPollingClient = $this->verifyClient($client);
     }
 }
