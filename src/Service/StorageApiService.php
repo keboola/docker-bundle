1b4e98e0472e5d3a099ce0bf90f9eb4c44c3d046
Odin <odinuv@gmail.com>
2018-08-21 14:26:23 +0200

    refactor: wip remove syrup dependency

--- src/Service/StorageApiService.php
+++ src/Service/StorageApiService.php
@@ -2,7 +2,12 @@
 
 namespace Keboola\DockerBundle\Service;
 
+use Keboola\DockerBundle\Exception\ApplicationException;
+use Keboola\DockerBundle\Exception\UserException;
 use Keboola\StorageApi\Client;
+use Keboola\StorageApi\ClientException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -13,11 +18,25 @@ class StorageApiService
      */
     private $fasterPollingClient = null;
 
+    /** @var RequestStack */
+    protected $requestStack;
+
+    /** @var Request */
+    protected $request;
     /**
      * @var LoggerInterface
      */
     private $logger;
 
+
+    /** @var Client */
+    protected $client;
+
+    protected $storageApiUrl;
+
+    protected $tokenData;
+
+    public function __construct(RequestStack $requestStack, $storageApiUrl = 'https://connection.keboola.com')
     /**
      * @var Client
      */
@@ -32,7 +51,104 @@ class StorageApiService
     /**
      * @return \Closure
      */
-    public function getStepPollDelayFunction()
+    public static function getStepPollDelayFunction()
+    {
+        $this->storageApiUrl = $storageApiUrl;
+        $this->requestStack = $requestStack;
+    }
+
+    protected function verifyClient(Client $client)
+    {
+        try {
+            $this->tokenData = $client->verifyToken();
+            return $client;
+        } catch (ClientException $e) {
+            if ($e->getCode() == 401) {
+                throw new UserException("Invalid StorageApi Token", $e);
+            } elseif ($e->getCode() < 500) {
+                throw new UserException($e->getMessage(), $e);
+            }
+            throw $e;
+        }
+    }
+
+    public function getBackoffTries($hostname)
+    {
+        // keep the backoff settings minimal for API servers
+        if (false === strstr($hostname, 'worker')) {
+            return 3;
+        }
+
+        return 11;
+    }
+
+    public function setClient(Client $client)
+    {
+        $this->client = $this->verifyClient($client);
+    }
+
+    public function getClient()
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        if ($this->client == null) {
+            if ($request == null) {
+                throw new UserException('no request');
+            }
+
+            if (!$request->headers->has('X-StorageApi-Token')) {
+                throw new UserException('Missing StorageAPI token');
+            }
+
+            $this->setClient(
+                new Client(
+                    [
+                        'token' => $request->headers->get('X-StorageApi-Token'),
+                        'url' => $this->storageApiUrl,
+                        'userAgent' => explode('/', $request->getPathInfo())[1],
+                        'backoffMaxTries' => $this->getBackoffTries(gethostname())
+                    ]
+                )
+            );
+
+            if ($request->headers->has('X-KBC-RunId')) {
+                $this->client->setRunId($request->headers->get('X-KBC-RunId'));
+            }
+        }
+
+        $client = $this->client;
+        if (!$this->fasterPollingClient) {
+            $clientWithFasterPolling = new Client(
+                [
+                    'token' => $client->token,
+                    'url' => $client->getApiUrl(),
+                    'userAgent' => $client->getUserAgent(),
+                    'backoffMaxTries' => $client->getBackoffMaxTries(),
+                    'jobPollRetryDelay' => self::getStepPollDelayFunction(),
+                    'logger' => $this->logger
+                ]
+            );
+            if ($client->getRunId()) {
+                $clientWithFasterPolling->setRunId($client->getRunId());
+            }
+            $this->setFasterPollingClient($clientWithFasterPolling);
+        }
+        return $this->fasterPollingClient;
+    }
+
+
+    public function getTokenData()
+    {
+        if ($this->tokenData == null) {
+            throw new ApplicationException('StorageApi Client was not initialized');
+        }
+        return $this->tokenData;
+    }
+
+    /**
+     * @return \Closure
+     */
+    public static function getStepPollDelayFunction()
     {
         return function ($tries) {
             switch ($tries) {
@@ -46,36 +162,6 @@ class StorageApiService
         };
     }
 
-    public function getLogger()
-    {
-        return $this->logger;
-    }
-
-    /**
-     * @return Client
-     */
-    public function getClient()
-    {
-        $client = parent::getClient();
-        if (!$this->fasterPollingClient) {
-            $clientWithFasterPolling = new Client(
-                [
-                    'token' => $client->token,
-                    'url' => $client->getApiUrl(),
-                    'userAgent' => $client->getUserAgent(),
-                    'backoffMaxTries' => $client->getBackoffMaxTries(),
-                    'jobPollRetryDelay' => $this->getStepPollDelayFunction(),
-                    'logger' => $this->logger
-                ]
-            );
-            if ($client->getRunId()) {
-                $clientWithFasterPolling->setRunId($client->getRunId());
-            }
-            $this->setFasterPollingClient($clientWithFasterPolling);
-        }
-        return $this->fasterPollingClient;
-    }
-
     public function getClientWithoutLogger()
     {
         $client = parent::getClient();
@@ -86,7 +172,7 @@ class StorageApiService
                     'url' => $client->getApiUrl(),
                     'userAgent' => $client->getUserAgent(),
                     'backoffMaxTries' => $client->getBackoffMaxTries(),
-                    'jobPollRetryDelay' => $this->getStepPollDelayFunction(),
+                    'jobPollRetryDelay' => self::getStepPollDelayFunction(),
                 ]
             );
             if ($client->getRunId()) {
