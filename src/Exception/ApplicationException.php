40bc565571c1a21242335ca284af354059db8f6a
Odin <odinuv@gmail.com>
2018-10-07 23:51:45 +0200

    fix: remove syrup dependency

--- src/Exception/ApplicationException.php
+++ src/Exception/ApplicationException.php
@@ -8,7 +8,7 @@ class ApplicationException extends \Exception
 
     public function __construct($message, $previous = null, array $data = [])
     {
-        parent::__construct($message, $previous, $data);
+        parent::__construct($message, 0, $previous);
         $this->data = $data;
     }
 
