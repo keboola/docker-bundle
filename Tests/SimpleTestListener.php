d37a8337666c553044fd4f46e0e154fd79ab2296
odin <odinuv@gmail.com>
2019-04-29 22:56:18 +0200

    debug

---
+++ Tests/SimpleTestListener.php
@@ -0,0 +1,14 @@
+<?php
+
+class SimpleTestListener extends PHPUnit_Framework_BaseTestListener
+{
+    public function endTest(PHPUnit_Framework_Test $test, $time)
+    {
+        printf(
+            "Test '%s' ended in %s m %s s.\n",
+            $test->getName(),
+            round(floor($time / 60)),
+            round($time - floor($time / 60))
+        );
+    }
+}
