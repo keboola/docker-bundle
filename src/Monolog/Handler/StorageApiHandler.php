06637791eee2233997ca0157d6e876214b0bb520
odin <odinuv@gmail.com>
2019-05-24 09:43:16 +0200

    refactor: split StorageApiHandler into class and interface

--- src/Monolog/Handler/StorageApiHandler.php
+++ src/Monolog/Handler/StorageApiHandler.php
@@ -14,28 +14,8 @@ use Symfony\Component\DependencyInjection\Container;
  * Class StorageApiHandler
  * @package Keboola\DockerBundle\Monolog\Handler
  */
-class StorageApiHandler extends \Monolog\Handler\AbstractHandler
+class StorageApiHandler extends \Monolog\Handler\AbstractHandler implements StorageApiHandlerInterface
 {
-    /**
-     * Verbosity None - event will not be stored in Storage at all.
-     */
-    const VERBOSITY_NONE = 'none';
-
-    /**
-     * Verbosity Camouflage - event will be stored in Storage only as a generic message.
-     */
-    const VERBOSITY_CAMOUFLAGE = 'camouflage';
-
-    /**
-     * Verbosity Normal - event will be stored in Storage as received.
-     */
-    const VERBOSITY_NORMAL = 'normal';
-
-    /**
-     * Verbosity Verbose - event will be stored in Storage including all additonal event data.
-     */
-    const VERBOSITY_VERBOSE = 'verbose';
-
     /**
      * @var array
      */
@@ -77,8 +57,7 @@ class StorageApiHandler extends \Monolog\Handler\AbstractHandler
     }
 
     /**
-     * Set verbosity for each error level. If a level is not provided, its verbosity will not be changed.
-     * @param array $verbosity Key is Monolog error level, value is verbosity constant.
+     * @inheritDoc
      */
     public function setVerbosity(array $verbosity)
     {
@@ -88,8 +67,7 @@ class StorageApiHandler extends \Monolog\Handler\AbstractHandler
     }
 
     /**
-     * Get verbosity for each error level.
-     * @return array Key is Monolog error level, value is verbosity constant.
+     * @inheritDoc
      */
     public function getVerbosity()
     {
