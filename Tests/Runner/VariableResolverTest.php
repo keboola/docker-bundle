0fe22a867d6672ade7f575bb88b4799006655fdb
odin <odinuv@gmail.com>
2020-03-14 08:06:27 +0100

    wip tests

--- Tests/Runner/VariableResolverTest.php
+++ Tests/Runner/VariableResolverTest.php
@@ -2,8 +2,24 @@
 
 namespace Keboola\DockerBundle\Docker;
 
+use Keboola\StorageApi\Client;
+use PHPUnit\Framework\TestCase;
 
-class VariableResolverTest
+class VariableResolverTest extends TestCase
 {
-
+    public function testResolveVariables()
+    {
+        $client = new Client([
+            'url' => STORAGE_API_URL,
+            'token' => STORAGE_API_TOKEN,
+        ]);
+        $variableResolver = new VariableResolver($client);
+        $jobDefinition = new JobDefinition(['foo' => 'bar'], new Component(['a' => 'b']), '123', '234', [], '123', false);
+        /** @var JobDefinition $newJobDefinition */
+        $newJobDefinition = $variableResolver->resolveVariables([$jobDefinition], '123', [])[0];
+        self::assertEquals(
+            [],
+            $newJobDefinition->getConfiguration()
+        );
+    }
 }
