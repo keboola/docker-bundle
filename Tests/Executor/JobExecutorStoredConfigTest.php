8437061bb91013fe6f9ed55bfb5197b142d71e3e
odin <odinuv@gmail.com>
2020-11-20 21:28:12 +0100

    tests: fix assertions

--- Tests/Executor/JobExecutorStoredConfigTest.php
+++ Tests/Executor/JobExecutorStoredConfigTest.php
@@ -5,6 +5,7 @@ namespace Keboola\DockerBundle\Tests\Functional;
 use Keboola\Csv\CsvFile;
 use Keboola\DockerBundle\Tests\BaseExecutorTest;
 use Keboola\StorageApi\Client;
+use Keboola\Syrup\Exception\UserException;
 use Keboola\Syrup\Job\Metadata\Job;
 
 class JobExecutorStoredConfigTest extends BaseExecutorTest
@@ -183,12 +184,6 @@ class JobExecutorStoredConfigTest extends BaseExecutorTest
                 ],
             ],
         ];
-        $csv = new CsvFile($this->getTemp()->getTmpFolder() . DIRECTORY_SEPARATOR . "upload.csv");
-        $csv->writeRow(['name', 'oldValue', 'newValue']);
-        $csv->writeRow(['price', '100', '1000']);
-        $csv->writeRow(['size', 'small', 'big']);
-        $this->getClient()->createTableAsync("in.c-executor-test", "source", $csv);
-
         $data = [
             'params' => [
                 'component' => 'keboola.python-transformation',
@@ -200,30 +195,8 @@ class JobExecutorStoredConfigTest extends BaseExecutorTest
         $jobExecutor = $this->getJobExecutor($configuration, []);
         $job = new Job($this->getEncryptorFactory()->getEncryptor(), $data);
         $job->setId(123456);
-        $ret = $jobExecutor->execute($job);
-        self::assertArrayHasKey('message', $ret);
-        self::assertArrayHasKey('images', $ret);
-        self::assertArrayHasKey('configVersion', $ret);
-
-        $csvData = $this->getClient()->getTableDataPreview('out.c-executor-test.output');
-        $data = Client::parseCsv($csvData);
-        usort($data, function ($a, $b) {
-            return strcmp($a['name'], $b['name']);
-        });
-        self::assertEquals(
-            [
-                [
-                    'name' => 'price',
-                    'oldValue' => '100',
-                    'newValue' => '1000',
-                ],
-                [
-                    'name' =>  'size',
-                    'oldValue' => 'small',
-                    'newValue' => 'big',
-                ],
-            ],
-            $data
-        );
+        self::expectException(UserException::class);
+        self::expectExceptionMessage('Error reading configuration \'executor-configuration\': Branch id "my-non-existent-branch" does not exist');
+        $jobExecutor->execute($job);
     }
 }
