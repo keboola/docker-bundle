9f5dfa34a893a40e6e55d437b510a906000b4162
odin <odinuv@gmail.com>
2021-08-27 14:27:12 +0200

    feat: add exasol workspace support

---
+++ Tests/backend-specific-tests/Exasol/RunnerExasolTest.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Keboola\DockerBundle\Tests\Runner;
+
+use Keboola\Csv\CsvFile;
+use Keboola\DockerBundle\Docker\Component;
+use Keboola\DockerBundle\Docker\JobDefinition;
+use Keboola\DockerBundle\Docker\Runner\UsageFile\NullUsageFile;
+use Keboola\DockerBundle\Tests\BaseRunnerTest;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\ClientException;
+use Keboola\StorageApi\Components;
+use Keboola\StorageApi\Options\Components\Configuration;
+use Keboola\StorageApi\Options\Components\ListComponentConfigurationsOptions;
+use Keboola\StorageApi\Options\Components\ListConfigurationWorkspacesOptions;
+use Keboola\StorageApi\Options\FileUploadOptions;
+use Keboola\StorageApi\Options\ListFilesOptions;
+use Keboola\StorageApi\Workspaces;
+use Keboola\Temp\Temp;
+
+class RunnerExasolTest extends BaseRunnerTest
+{
+    const ABS_TEST_FILE_TAG = 'abs-workspace-runner-test';
+
+    private function clearBuckets()
+    {
+        foreach (['in.c-exasol-runner-test', 'out.c-exasol-runner-test'] as $bucket) {
+            try {
+                $this->getClient()->dropBucket($bucket, ['force' => true]);
+            } catch (ClientException $e) {
+                if ($e->getCode() != 404) {
+                    throw $e;
+                }
+            }
+        }
+    }
+
+    private function clearConfigs()
+    {
+        $componentsApi = new Components($this->client);
+        $configurations = $componentsApi->listComponentConfigurations(
+            (new ListComponentConfigurationsOptions())->setComponentId('keboola.runner-workspace-exasol-test')
+        );
+        $workspacesApi = new Workspaces($this->client);
+        foreach ($configurations as $configuration) {
+            $workspaces = $componentsApi->listConfigurationWorkspaces(
+                (new ListConfigurationWorkspacesOptions())
+                    ->setComponentId('keboola.runner-workspace-exasol-test')
+                    ->setConfigurationId($configuration['id'])
+            );
+            foreach ($workspaces as $workspace) {
+                $workspacesApi->deleteWorkspace($workspace['id']);
+            }
+            $componentsApi->deleteConfiguration('keboola.runner-workspace-exasol-test', $configuration['id']);
+        }
+    }
+
+    protected function initStorageClient()
+    {
+        $this->client = new Client([
+            'url' => STORAGE_API_URL_EXASOL,
+            'token' => STORAGE_API_TOKEN_EXASOL,
+        ]);
+    }
+
+    public function setUp()
+    {
+        if (!RUN_EXASOL_TESTS) {
+            self::markTestSkipped('Exasol test is disabled.');
+        }
+        parent::setUp();
+    }
+
+    private function createBuckets()
+    {
+        $this->clearBuckets();
+        // Create buckets
+        $this->getClient()->createBucket('exasol-runner-test', Client::STAGE_IN, 'Docker TestSuite', 'exasol');
+        $this->getClient()->createBucket('exasol-runner-test', Client::STAGE_OUT, 'Docker TestSuite', 'exasol');
+    }
+
+    /**
+     * @param array $componentData
+     * @param $configId
+     * @param array $configData
+     * @param array $state
+     * @return JobDefinition[]
+     */
+    protected function prepareJobDefinitions(array $componentData, $configId, array $configData, array $state)
+    {
+        $jobDefinition = new JobDefinition($configData, new Component($componentData), $configId, 'v123', $state);
+        return [$jobDefinition];
+    }
+
+    public function testWorkspaceExasolMapping()
+    {
+        $this->clearBuckets();
+        $this->createBuckets();
+        $this->clearConfigs();
+        $temp = new Temp();
+        $temp->initRunFolder();
+        $csv = new CsvFile($temp->getTmpFolder() . '/upload.csv');
+        $csv->writeRow(['id', 'text']);
+        $csv->writeRow(['test1', 'test1']);
+        $this->getClient()->createTableAsync('in.c-exasol-runner-test', 'mytable', $csv);
+        unset($csv);
+
+        $componentData = [
+            'id' => 'keboola.runner-workspace-exasol-test',
+            'data' => [
+                'definition' => [
+                    'type' => 'aws-ecr',
+                    'uri' => '061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola.runner-workspace-test',
+                    'tag' => 'latest',
+                ],
+                'staging_storage' => [
+                    'input' => 'workspace-exasol',
+                    'output' => 'workspace-exasol',
+                ],
+            ],
+        ];
+
+        $configId = uniqid('runner-test-');
+        $components = new Components($this->client);
+        $configuration = new Configuration();
+        $configuration->setComponentId('keboola.runner-workspace-exasol-test');
+        $configuration->setName('runner-tests');
+        $configuration->setConfigurationId($configId);
+        $components->addConfiguration($configuration);
+        $runner = $this->getRunner();
+
+        self::assertFalse($this->client->tableExists('out.c-exasol-runner-test.new-table'));
+        $runner->run(
+            $this->prepareJobDefinitions(
+                $componentData,
+                $configId,
+                [
+                    'storage' => [
+                        'input' => [
+                            'tables' => [
+                                [
+                                    'source' => 'in.c-exasol-runner-test.mytable',
+                                    'destination' => 'local-table',
+                                ],
+                            ],
+                        ],
+                        'output' => [
+                            'tables' => [
+                                [
+                                    'source' => 'local-table-out',
+                                    'destination' => 'out.c-exasol-runner-test.new-table',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'parameters' => [
+                        'operation' => 'copy-exasol',
+                    ],
+                ],
+                []
+            ),
+            'run',
+            'run',
+            '1234567',
+            new NullUsageFile()
+        );
+
+        $options = new ListConfigurationWorkspacesOptions();
+        $options->setComponentId('keboola.runner-workspace-exasol-test');
+        $options->setConfigurationId($configId);
+        self::assertCount(0, $components->listConfigurationWorkspaces($options));
+        self::assertTrue($this->client->tableExists('out.c-exasol-runner-test.new-table'));
+    }
+}
