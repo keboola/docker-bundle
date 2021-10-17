4cbd2848a6bc5b2aa965dc315b1aab4860b14838
odin <odinuv@gmail.com>
2021-10-17 21:24:22 +0200

    chore: fix cs

--- Tests/RunnerPart2/SharedCodeResolverTest.php
+++ Tests/RunnerPart2/SharedCodeResolverTest.php
@@ -48,7 +48,7 @@ class SharedCodeResolverTest extends TestCase
         return [$configId, $rowIds];
     }
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
