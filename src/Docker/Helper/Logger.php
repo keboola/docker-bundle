a853a2d3615f67de212d09fc88a171b6f0c3f40b
Erik Zigo <erik.zigo@keboola.com>
2024-01-12 10:23:02 +0100

    Docker/Helper/Logger remove unused imports

--- src/Docker/Helper/Logger.php
+++ src/Docker/Helper/Logger.php
@@ -4,23 +4,6 @@ declare(strict_types=1);
 
 namespace Keboola\DockerBundle\Docker\Helper;
 
-use Keboola\DockerBundle\Docker\Runner\DataLoader\DataLoaderInterface;
-use Keboola\DockerBundle\Docker\Runner\Output;
-use Keboola\InputMapping\Table\Result\Column as ColumnInfo;
-use Keboola\InputMapping\Table\Result\TableInfo;
-use Keboola\InputMapping\Table\Result\TableMetrics as InputTableMetrics;
-use Keboola\JobQueueInternalClient\JobFactory\Runtime\Backend;
-use Keboola\JobQueueInternalClient\Result\InputOutput\Column;
-use Keboola\JobQueueInternalClient\Result\InputOutput\ColumnCollection;
-use Keboola\JobQueueInternalClient\Result\InputOutput\Table;
-use Keboola\JobQueueInternalClient\Result\InputOutput\TableCollection;
-use Keboola\JobQueueInternalClient\Result\JobArtifacts;
-use Keboola\JobQueueInternalClient\Result\JobMetrics;
-use Keboola\JobQueueInternalClient\Result\JobResult;
-use Keboola\JobQueueInternalClient\Result\Variable\Variable;
-use Keboola\JobQueueInternalClient\Result\Variable\VariableCollection;
-use Keboola\OutputMapping\Table\Result\TableMetrics as OutputTableMetrics;
-
 class Logger
 {
     public const MAX_LENGTH = 4000;
