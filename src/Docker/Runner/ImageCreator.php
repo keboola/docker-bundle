d31bf00c0c0250a5a3215b43b33c55f910b47c34
Erik Zigo <erik.zigo@keboola.com>
2024-10-02 12:21:23 +0200

    Runner/ImageCreator codestyle

--- src/Docker/Runner/ImageCreator.php
+++ src/Docker/Runner/ImageCreator.php
@@ -9,49 +9,32 @@ use Keboola\DockerBundle\Docker\Image;
 use Keboola\DockerBundle\Docker\ImageFactory;
 use Keboola\DockerBundle\Exception\UserException;
 use Keboola\StorageApi\BranchAwareClient;
-use Keboola\StorageApi\Client;
 use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\Components;
-use Keboola\Temp\Temp;
 use Psr\Log\LoggerInterface;
 
 class ImageCreator
 {
-    private LoggerInterface $logger;
-    private BranchAwareClient $storageClient;
-    private Component $mainComponent;
-    private array $componentConfig;
-
     private array $before;
     private array $after;
-    private Temp $temp;
 
     public function __construct(
-        LoggerInterface $logger,
-        BranchAwareClient $storageClient,
-        Component $mainComponent,
-        array $componentConfig,
+        private readonly LoggerInterface $logger,
+        private readonly BranchAwareClient $storageClient,
+        private readonly Component $mainComponent,
+        private readonly array $componentConfig,
     ) {
-        $this->logger = $logger;
-        $this->mainComponent = $mainComponent;
-        $this->storageClient = $storageClient;
-        $this->before = empty($componentConfig['processors']['before']) ? [] : $componentConfig['processors']['before'];
-        $this->after = empty($componentConfig['processors']['after']) ? [] : $componentConfig['processors']['after'];
-        $this->componentConfig = $componentConfig;
-        $this->temp = new Temp();
+        $this->before = $componentConfig['processors']['before'] ?: [];
+        $this->after = $componentConfig['processors']['after'] ?: [];
     }
 
     /**
      * @return Image[]
      */
-    public function prepareImages()
+    public function prepareImages(): array
     {
         foreach ($this->before as $processor) {
-            $componentId = $processor['definition']['component'];
-            $component = $this->getComponent($componentId);
-            $image = ImageFactory::getImage($this->logger, $component, false);
-            $image->prepare(['parameters' => empty($processor['parameters']) ? [] : $processor['parameters']]);
-            $images[] = $image;
+            $images[] = $this->prepareImageForProcessor($processor);
         }
 
         $image = ImageFactory::getImage($this->logger, $this->mainComponent, true);
@@ -59,21 +42,13 @@ class ImageCreator
         $images[] = $image;
 
         foreach ($this->after as $processor) {
-            $componentId = $processor['definition']['component'];
-            $component = $this->getComponent($componentId);
-            $image = ImageFactory::getImage($this->logger, $component, false);
-            $image->prepare(['parameters' => empty($processor['parameters']) ? [] : $processor['parameters']]);
-            $images[] = $image;
+            $images[] = $this->prepareImageForProcessor($processor);
         }
 
         return $images;
     }
 
-    /**
-     * @param string $id
-     * @return Component
-     */
-    protected function getComponent($id)
+    private function getComponent(string $id): Component
     {
         $componentsApi = new Components($this->storageClient);
         try {
@@ -82,5 +57,14 @@ class ImageCreator
         } catch (ClientException $e) {
             throw new UserException(sprintf('Cannot get component "%s": %s.', $id, $e->getMessage()), $e);
         }
+    }
+
+    private function prepareImageForProcessor(array $processorData): Image
+    {
+        $componentId = $processorData['definition']['component'];
+        $component = $this->getComponent((string) $componentId);
+        $image = ImageFactory::getImage($this->logger, $component, false);
+        $image->prepare(['parameters' => $processorData['parameters'] ?: []]);
+        return $image;
     }
 }
