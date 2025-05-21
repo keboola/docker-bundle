0b3283701aa7c6b3a32da5eb85fbe2a73fb48f22
Pepa Martinec <josef.martinec@keboola.com>
2025-05-21 15:05:10 +0100

    feat[data-loader]: Make staging workspace non-lazy

--- src/Docker/Runner/DataLoader/OutputDataLoaderFactory.php
+++ src/Docker/Runner/DataLoader/OutputDataLoaderFactory.php
@@ -4,23 +4,13 @@ declare(strict_types=1);
 
 namespace Keboola\DockerBundle\Docker\Runner\DataLoader;
 
-use Keboola\InputMapping\Staging\StrategyFactory as InputStrategyFactory;
 use Keboola\JobQueue\JobConfiguration\JobDefinition\Component\ComponentSpecification;
 use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Configuration;
-use Keboola\JobQueue\JobConfiguration\Mapping\InputDataLoader;
 use Keboola\JobQueue\JobConfiguration\Mapping\OutputDataLoader;
 use Keboola\OutputMapping\Staging\StrategyFactory as OutputStrategyFactory;
 use Keboola\StagingProvider\Staging\File\FileFormat;
-use Keboola\StagingProvider\Staging\File\LocalStaging;
-use Keboola\StagingProvider\Staging\StagingProvider;
 use Keboola\StagingProvider\Staging\StagingType;
-use Keboola\StagingProvider\Staging\Workspace\LazyWorkspaceStaging;
-use Keboola\StagingProvider\Staging\Workspace\WorkspaceStagingInterface;
-use Keboola\StagingProvider\Workspace\ProviderConfig\ExistingWorkspaceConfig;
-use Keboola\StagingProvider\Workspace\WorkspaceProvider;
 use Keboola\StorageApiBranch\ClientWrapper;
-use LogicException;
-use Psr\Log\LoggerInterface;
 
 class OutputDataLoaderFactory extends BaseDataLoaderFactory
 {
--- src/Docker/Runner/DataLoader/StagingWorkspaceFacade.php
+++ src/Docker/Runner/DataLoader/StagingWorkspaceFacade.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace Keboola\DockerBundle\Docker\Runner\DataLoader;
 
-use Keboola\StagingProvider\Exception\NoStagingAvailableException;
-use Keboola\StagingProvider\Staging\StagingProvider;
-use Keboola\StagingProvider\Staging\Workspace\LazyWorkspaceStaging;
-use Keboola\StagingProvider\Workspace\WorkspaceInterface;
 use Keboola\StagingProvider\Workspace\WorkspaceProvider;
 use Keboola\StagingProvider\Workspace\WorkspaceWithCredentialsInterface;
 use Psr\Log\LoggerInterface;
@@ -17,73 +13,40 @@ class StagingWorkspaceFacade
 {
     public function __construct(
         private readonly WorkspaceProvider $workspaceProvider,
-        private readonly StagingProvider $stagingProvider,
         private readonly LoggerInterface $logger,
+        private readonly WorkspaceWithCredentialsInterface $workspace,
+        private readonly bool $isReusable,
     ) {
     }
 
     public function getWorkspaceId(): ?string
     {
-        return $this->getRealWorkspace()?->getWorkspaceId();
+        return $this->workspace->getWorkspaceId();
     }
 
     public function getBackendSize(): ?string
     {
-        return $this->getRealWorkspace()?->getBackendSize();
+        return $this->workspace->getBackendSize();
     }
 
     public function getCredentials(): array
     {
-        $workspace = $this->getRealWorkspace();
-        if (!$workspace instanceof WorkspaceWithCredentialsInterface) {
-            return [];
-        }
-
-        return $workspace->getCredentials();
+        return $this->workspace->getCredentials();
     }
 
     public function cleanup(): void
     {
-        $workspace = $this->getLazyWorkspace();
-        if ($workspace === null) {
-            return;
-        }
-
-        if ($workspace instanceof LazyWorkspaceStaging && !$workspace->isInitialized()) {
+        if ($this->isReusable) {
             return;
         }
 
         try {
-            $this->workspaceProvider->cleanupWorkspace($workspace->getWorkspaceId());
+            $this->workspaceProvider->cleanupWorkspace($this->workspace->getWorkspaceId());
         } catch (Throwable $e) {
             // ignore errors if the cleanup fails because we a) can't fix it b) should not break the job
             $this->logger->error('Failed to cleanup workspace: ' . $e->getMessage(), [
                 'exception' => $e,
             ]);
         }
-    }
-
-    private function getLazyWorkspace(): ?WorkspaceInterface
-    {
-        try {
-            $staging = $this->stagingProvider->getTableDataStaging();
-        } catch (NoStagingAvailableException) {
-            return null;
-        }
-
-        if (!$staging instanceof WorkspaceInterface) {
-            return null;
-        }
-
-        return $staging;
-    }
-
-    private function getRealWorkspace(): ?WorkspaceInterface
-    {
-        $workspace = $this->getLazyWorkspace();
-        while ($workspace instanceof LazyWorkspaceStaging) {
-            $workspace = $workspace->getWorkspace();
-        }
-        return $workspace;
     }
 }
